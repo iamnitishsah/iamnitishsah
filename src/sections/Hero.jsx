d8e690296f7aa8d0ca38f310ddb984e3fca557e0
Nitish Kumar <iamnitishsah12@gmail.com>
2026-04-14 17:15:40 +0530

    Change profile image format from jpg to jpeg

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import useTypewriter from "../hooks/useTypewriter";
 import { profile } from "../data/profile";
 import Button from "../components/common/Button";
-import profileImg from "../assets/images/Official.jpg";
+import profileImg from "../assets/images/Official.jpeg";
 
 export default function Hero() {
     const typedText = useTypewriter(profile.speciality);
